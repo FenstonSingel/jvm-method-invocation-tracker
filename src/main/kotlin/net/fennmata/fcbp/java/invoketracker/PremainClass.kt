bb154972f0ba1d13c6abc3dd50aeeda645c21cae
FenstonSingel <fenstonsingel@gmail.com>
2022-07-17 18:51:37 +0300

    Fixed the classic "multiple Java agent installations" Gradle-related bug

--- src/main/kotlin/net/fennmata/fcbp/java/invoketracker/PremainClass.kt
+++ src/main/kotlin/net/fennmata/fcbp/java/invoketracker/PremainClass.kt
@@ -1,15 +1,23 @@
 package net.fennmata.fcbp.java.invoketracker
 
 import java.lang.instrument.Instrumentation
+import java.util.concurrent.atomic.AtomicBoolean
 
 @Suppress("UNUSED")
 object PremainClass {
 
     @JvmStatic
     fun premain(argumentsString: String?, instrumentation: Instrumentation) {
+        // this is a godforsaken workaround motivated by
+        // a bug in IDEA's Gradle run delegation feature
+        // that runs all(?) premain methods twice
+        if (hasAgentAlreadyBeenStarted.getAndSet(true)) return
+
         with(instrumentation) {
             addTransformer(MITClassFileTransformer, isRetransformClassesSupported)
         }
     }
+
+    private var hasAgentAlreadyBeenStarted = AtomicBoolean(false)
 
 }
