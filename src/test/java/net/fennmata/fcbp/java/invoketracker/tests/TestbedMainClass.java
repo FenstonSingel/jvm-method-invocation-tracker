42dea1ab7dbbdaa5be39747e10a542617db7a900
FenstonSingel <fenstonsingel@gmail.com>
2022-07-17 18:56:18 +0300

    Tweaked the testbed's warning suppression a bit

--- src/test/java/net/fennmata/fcbp/java/invoketracker/tests/TestbedMainClass.java
+++ src/test/java/net/fennmata/fcbp/java/invoketracker/tests/TestbedMainClass.java
@@ -1,6 +1,7 @@
 package net.fennmata.fcbp.java.invoketracker.tests;
 
 // TODO write actual functional tests for the JVM method invocation tracker
+@SuppressWarnings("unused")
 public class TestbedMainClass {
 
     public static void main(String[] args) {
